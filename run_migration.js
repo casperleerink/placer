3552d5d10c287dacbdce042e466a54693f85ac87
marcleerink <marc.leerink@planet.com>
2023-07-22 10:12:59 +0200

    update schema unique contraints and indexes

--- run_migration.js
+++ run_migration.js
@@ -3,6 +3,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 
+
 config.config();
 
 const sql = postgres(process.env.DATABASE_URL, {max: 1})
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,22 +1,22 @@
-import { index, integer, json, pgTable, primaryKey, serial, text, varchar } from "drizzle-orm/pg-core";
+import { integer, json, pgTable, primaryKey, serial, uniqueIndex, varchar } from "drizzle-orm/pg-core";
 
 export const users = pgTable('users', {
   id: serial('id').primaryKey(),
-  fullName: text('full_name'),
-  email: varchar('email', {length: 255}),
+  fullName: varchar('full_name', {length: 255}),
+  email: varchar('email', {length: 255}).unique(),
 });
 
 export const posts = pgTable('posts', {
     id: serial('id').primaryKey(),
     title: varchar('title').notNull(),
     content: json('content'),
-    authorId: varchar('author_id', {length: 255}),
+    authorId: integer('author_id').references(() => users.id).notNull(),
     x: integer('x',).notNull(),
     y: integer('y').notNull(),
 }, (table) => {
     return {
-        index_x: index("index_x").on(table.x),
-        index_y: index("index_y").on(table.y),
+        index_x: uniqueIndex("index_x").on(table.x),
+        index_y: uniqueIndex("index_y").on(table.y),
     };
 });
 

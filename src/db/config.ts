35a72c1ce31183fba6dbe7542ab4d088a56b17d6
marcleerink <marc.leerink@planet.com>
2023-07-23 10:49:35 +0200

    prettier format

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,6 +1,5 @@
 import { drizzle, type PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-
 const dbUrl = process.env.DATABASE_URL as string;
 // const migrationClient = postgres(dbUrl, { max: 1 });
 // migrate(drizzle(migrationClient), './migrations');
